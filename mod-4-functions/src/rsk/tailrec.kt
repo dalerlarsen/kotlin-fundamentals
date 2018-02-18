971cca168f8bcb44f0290c457166c9bb0eb16951
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-18 08:45:04 -0500

    Tail Recursive Functions

---
+++ mod-4-functions/src/rsk/tailrec.kt
@@ -0,0 +1,17 @@
+package rsk
+
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+    println(fib(10000, BigInteger("1"), BigInteger("0")))
+}
+
+tailrec fun fib(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+    return if (n == 0) b else fib(n - 1, a + b, a)
+}
+
+//fun fib(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+//    return if (n == 0) b else fib(n - 1, a + b, a)
+//}
+
+// 1, 1, 2, 3, 5, 8, 13, 21...
