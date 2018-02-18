806ed9ac17cde0689a73a2814978f433ae662f89
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-18 08:25:06 -0500

    Extension Functions

---
+++ mod-4-functions/src/rsk/Extension.kt
@@ -0,0 +1,18 @@
+package rsk
+
+fun main(args: Array<String>) {
+    val text = "With   multiple \t whitespace"
+    println(text)
+    println(replaceMultipleWhitespace(text))
+    println(text.replaceMultipleWhitespaceEx())
+}
+
+fun replaceMultipleWhitespace(value: String) : String {
+    var regex = Regex("\\s+")
+    return regex.replace(value, " ")
+}
+
+fun String.replaceMultipleWhitespaceEx() : String {
+    var regex = Regex("\\s+")
+    return regex.replace(this, " ")
+}
