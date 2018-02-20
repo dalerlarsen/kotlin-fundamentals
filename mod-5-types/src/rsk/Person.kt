b6ab191b39d810d0d0df375c69fd8f392586f5ac
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-20 13:04:17 -0500

    Providing Constructors

--- mod-5-types/src/rsk/Person.kt
+++ mod-5-types/src/rsk/Person.kt
@@ -1,5 +1,13 @@
 package rsk
 
+fun main(args: Array<String>) {
+    val dale = Student("Dale", "Larsen", 1)
+    val kevin = Student("Kevin", "Jones", 2, "Somebody")
+
+    println(kevin.id)
+}
+
+
 //open class Person {
 //    var firstName: String = ""
 //    var lastName: String = ""
@@ -7,16 +15,29 @@ package rsk
 //    open fun getName() : String = "$firstName $lastName"
 //}
 
-abstract class Person {
-    var firstName: String = ""
-    var lastName: String = ""
+abstract class Person(var firstName: String, var lastName: String) {
 
     open fun getName() : String = "$firstName $lastName"
 
     abstract fun getAddress() : String
 }
 
-class Student : Person() {
+//class Student(val id: Int) : Person() {
+//class Student(_id: Int) : Person() {
+//    val id = _id
+
+// this is better than the secondary constructor
+class Student(firstName: String, lastName: String, _id: Int, tutor: String = "") : Person(firstName, lastName) {
+    val id: Int
+
+    init {
+        id = _id
+//        tutor = ""
+    }
+
+//    constructor(firstName: String, lastName: String, _id: Int, tutor: String) : this(firstName, lastName, _id) {
+//        this.tutor = tutor
+//    }
     override fun getAddress() : String {
         return ""
     }
