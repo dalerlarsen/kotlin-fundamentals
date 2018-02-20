58e6df4926daa10d4484f083c94c1d056334b3ad
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-20 12:14:08 -0500

    Sealed Classes

---
+++ mod-5-types/src/rsk/LightBulb.kt
@@ -0,0 +1,21 @@
+package rsk
+
+fun main(args: Array<String>) {
+    val someEvent: PersonEvent = PersonEvent.Eating("hotdog")
+
+    handlePersonEvent(someEvent)
+}
+
+sealed class PersonEvent {
+    class Awake : PersonEvent()
+    class Asleep : PersonEvent()
+    class Eating(val food: String) : PersonEvent()
+}
+
+fun handlePersonEvent(event: PersonEvent) {
+    when(event) {
+        is PersonEvent.Awake -> println("Awake")
+        is PersonEvent.Asleep -> println("Asleep")
+        is PersonEvent.Eating -> println("Eating ${event.food}")
+    }
+}
