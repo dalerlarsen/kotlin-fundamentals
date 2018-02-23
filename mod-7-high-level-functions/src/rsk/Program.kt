1f3fcc11946ad4f65b6cdbecbfafb676735b723b
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-23 12:33:42 -0500

    Anonymous Classes

---
+++ mod-7-high-level-functions/src/rsk/Program.kt
@@ -0,0 +1,51 @@
+package rsk
+
+fun main(args: Array<String>) {
+    var program = Program()
+
+//    program.fibonacci(8)
+    program.fibonacci(8, object : Process {
+        override fun execute(value: Int) {
+            println(value)
+        }
+    })
+
+}
+
+interface Process {
+    fun execute(value: Int)
+}
+
+class Program {
+    // 1,1,2,3,5,8,13,...
+
+    fun fibonacci(limit: Int, action: Process) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            action.execute(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
+
+    fun fibonacci(limit: Int) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            println(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
+}
