d76d52db1477a692bd1d82dd5aefb321fb00f287
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-23 12:20:46 -0500

    Companion Objects

--- mod-6-objects/src/rsk/Person.kt
+++ mod-6-objects/src/rsk/Person.kt
@@ -5,6 +5,24 @@ fun main(args: Array<String>) {
     val kevin = Student("Kevin", "Jones", 2, "Somebody")
 
     println(kevin.id)
+
+    Student.createPostgraduate("DalePost")
+    Student.createUndergraduate("DaleUnder")
+}
+
+class Program {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val dale = Student("Dale", "Larsen", 1)
+            val kevin = Student("Kevin", "Jones", 2, "Somebody")
+
+            println(kevin.id)
+
+            Student.createPostgraduate("DalePost")
+            Student.createUndergraduate("DaleUnder")
+        }
+    }
 }
 
 abstract class Person(var firstName: String, var lastName: String) {
@@ -15,7 +33,7 @@ abstract class Person(var firstName: String, var lastName: String) {
 }
 
 // this is better than the secondary constructor
-class Student(firstName: String, lastName: String, _id: Int, tutor: String = "") : Person(firstName, lastName) {
+open class Student(firstName: String, lastName: String, _id: Int, tutor: String = "") : Person(firstName, lastName) {
     val id: Int
 
     init {
@@ -33,4 +51,32 @@ class Student(firstName: String, lastName: String, _id: Int, tutor: String = "")
     }
 
     override fun getName() : String {return ""}
+
+    companion object : XmlSerialize<Student> {
+        override fun toXml(item: Student) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        fun createUndergraduate(name: String): Undergraduate {
+            return Undergraduate(name)
+        }
+         fun createPostgraduate(name: String): Postgraduate {
+            return Postgraduate(name)
+        }
+    }
 }
+
+class Undergraduate(firstName: String) : Student(firstName, "", 1) {
+
+}
+
+class Postgraduate(firstName: String) : Student(firstName, "", 1) {
+
+}
+
+interface XmlSerialize<T> {
+    fun toXml(item: T)
+}
+
+
+
