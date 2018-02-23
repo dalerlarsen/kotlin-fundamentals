e13bd6841fcc9c99b19bbe6ad3730b4814b75330
Dale Larsen <dale.larsen@pinnsg.com>
2018-02-23 12:42:40 -0500

    Higher Order Functions

--- mod-7-high-level-functions/src/rsk/Program.kt
+++ mod-7-high-level-functions/src/rsk/Program.kt
@@ -4,11 +4,23 @@ fun main(args: Array<String>) {
     var program = Program()
 
 //    program.fibonacci(8)
-    program.fibonacci(8, object : Process {
-        override fun execute(value: Int) {
-            println(value)
-        }
-    })
+//    program.fibonacci(8, object : Process {
+//        override fun execute(value: Int) {
+//            println(value)
+//        }
+//    })
+
+    program.fibonacci(8, {n -> println(n)})
+
+    // same thing below..different syntax
+    program.fibonacci(8) {n -> println(n)}
+
+    // even more reduced
+    program.fibonacci(8) {println(it)}
+
+    // even more reduced
+    program.fibonacci(8, ::println)
+
 
 }
 
@@ -18,6 +30,21 @@ interface Process {
 
 class Program {
     // 1,1,2,3,5,8,13,...
+
+    fun fibonacci(limit: Int, action: (Int) -> Unit) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            action(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
 
     fun fibonacci(limit: Int, action: Process) {
         var prev = 0
