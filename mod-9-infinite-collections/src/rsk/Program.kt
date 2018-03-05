46e7790bcb47b9fc0cade3526653e23b297489c0
Dale Larsen <dale.larsen@pinnsg.com>
2018-03-05 12:41:58 -0500

    Sequences

---
+++ mod-9-infinite-collections/src/rsk/Program.kt
@@ -0,0 +1,13 @@
+package rsk
+
+fun main(args: Array<String>) {
+
+    val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
+
+    val titles = meetings
+            .asSequence()
+            .filter { println("filter($it)" ); it.title.endsWith("g")}
+            .map { println("map($it)"); it.title }
+
+    for (t in titles) println(t)
+}
