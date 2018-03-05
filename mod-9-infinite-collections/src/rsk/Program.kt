721d0e0bd5ed82864332cf195d1d01e1966ca93f
Dale Larsen <dale.larsen@pinnsg.com>
2018-03-05 12:49:30 -0500

    Terminal operations on Sequences

--- mod-9-infinite-collections/src/rsk/Program.kt
+++ mod-9-infinite-collections/src/rsk/Program.kt
@@ -1,5 +1,9 @@
 package rsk
 
+fun iter(seq: Sequence<String>) {
+    for (t in seq) println(t)
+
+}
 fun main(args: Array<String>) {
 
     val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
@@ -9,5 +13,14 @@ fun main(args: Array<String>) {
             .filter { println("filter($it)" ); it.title.endsWith("g")}
             .map { println("map($it)"); it.title }
 
-    for (t in titles) println(t)
+    iter(titles)
+
+//    for (t in titles) println(t)
+//
+//    val titles2 = meetings
+//            .asSequence()
+//            .map { println("map($it)" );it.title.toUpperCase() }
+//            .find { it.startsWith("BOARD") }
+//
+//    println(titles2)
 }
