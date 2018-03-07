ee6619782fe0b2b76792c0ff07e715107392dd99
Dale Larsen <dale.larsen@pinnsg.com>
2018-03-07 12:15:35 -0500

    Use SAM Constructor

--- mod-10-java-functions/src/rsk/kotlin/Program.kt
+++ mod-10-java-functions/src/rsk/kotlin/Program.kt
@@ -1,11 +1,22 @@
 package rsk.kotlin
 
+import rsk.java.Created
 import rsk.java.User
 
 fun main(args: Array<String>) {
     var user = User("Kevin")
     var count = 0
 
-    user.create { println("User $it has been created: ${++count} times") }
+    // This works fine
+//    var eventListener = { u:User -> println("User $u has been created: ${++count} times") }
+//
+//    user.create { eventListener }
+//    user.create { eventListener }
+
+    // using a SAM constructor
+    var eventListener = Created({ println("User $it has been created: ${++count} times") })
+
+    user.create (eventListener)
+
     user.create { println("User $it has been created: ${++count} times") }
 }
